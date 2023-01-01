feb51afd2f9b24a98f826eb95ea1d03a685a3d62
Pete Birkinshaw <pete@binary-ape.org>
2023-01-01 21:33:44 +0000

    Benchmark the backends - xmlsectool is 1.78x slower

---
+++ test/verify_benchmark.exs
@@ -0,0 +1,14 @@
+## Run with `mix run test/benchmark.exs`
+
+src = Smee.Source.new("http://metadata.ukfederation.org.uk/ukfederation-metadata.xml", cert_file: "./ukfederation.pem")
+md = Smee.Fetch.remote(src)
+
+Benchee.run(
+  %{
+    "xmlsec1" => fn -> Smee.Security.Xmlsec1.verify!(md) end,
+    "xmlsectool" => fn -> Smee.Security.Xmlsectool.verify!(md) end
+  },
+  time: 30,
+  memory_time: 1,
+  parallel: 2
+)
