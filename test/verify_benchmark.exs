6cb48111f1c67489479583c324fdde4b59a37c4a
Pete Birkinshaw <pete@binary-ape.org>
2023-01-05 10:37:46 +0000

    Deleting MDQT and Erlang signing backends (for now)

--- test/verify_benchmark.exs
+++ test/verify_benchmark.exs
@@ -5,11 +5,10 @@ md = Smee.Fetch.remote(src)
 
 Benchee.run(
   %{
- #   "xmlsec1" => fn -> Smee.Security.Xmlsec1.verify!(md) end,
- #   "xmlsectool" => fn -> Smee.Security.Xmlsectool.verify!(md) end,
-    "mdqt" => fn -> Smee.Security.Mdqt.verify!(md) end
+    "xmlsec1" => fn -> Smee.Security.Xmlsec1.verify!(md) end,
+    "xmlsectool" => fn -> Smee.Security.Xmlsectool.verify!(md) end,
   },
-  time: 320,
+  time: 30,
   memory_time: 1,
   parallel: 2
 )
