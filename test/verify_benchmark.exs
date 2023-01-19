baa43af54b01776c0bd2ae00e1fb01e62d6aae64
Pete Birkinshaw <pete@binary-ape.org>
2023-01-19 21:31:24 +0000

    Update benchmark for MDQ (which isn't handled yet!)

--- test/verify_benchmark.exs
+++ test/verify_benchmark.exs
@@ -1,12 +1,16 @@
 ## Run with `mix run test/benchmark.exs`
 
-src = Smee.Source.new("http://metadata.ukfederation.org.uk/ukfederation-metadata.xml", cert_file: "./ukfederation.pem")
-md = Smee.Fetch.remote(src)
+big_src = Smee.Source.new("http://metadata.ukfederation.org.uk/ukfederation-metadata.xml", cert_file: "./ukfederation.pem", cert_fingerprint: "AD:80:7A:6D:26:8C:59:01:55:47:8D:F1:BA:61:68:10:DA:81:86:66")
+small_src = Smee.Source.new("http://mdq.ukfederation.org.uk/entities/https%3A%2F%2Findiid.net%2Fidp%2Fshibboleth", cert_file: "http://mdq.ukfederation.org.uk/ukfederation-mdq.pem", cert_fingerprint: nil)
+big_md = Smee.Fetch.remote(big_src)
+small_md = Smee.Fetch.remote(small_src)
 
 Benchee.run(
   %{
-    "xmlsec1" => fn -> Smee.Security.Xmlsec1.verify!(md) end,
-    "xmlsectool" => fn -> Smee.Security.Xmlsectool.verify!(md) end,
+    "xmlsec1_big" => fn -> Smee.Security.Xmlsec1.verify!(big_md) end,
+    "xmlsectool_big" => fn -> Smee.Security.Xmlsectool.verify!(big_md) end,
+#    "xmlsec1_small" => fn -> Smee.Security.Xmlsec1.verify!(small_md) end,
+#    "xmlsectool_small" => fn -> Smee.Security.Xmlsectool.verify!(small_md) end,
   },
   time: 30,
   memory_time: 1,
