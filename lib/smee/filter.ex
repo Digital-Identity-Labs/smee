a01941287f93bad3f2a41185bb9c9360fed86aee
Pete Birkinshaw <pete@binary-ape.org>
2023-01-23 18:39:26 +0000

    Sketch out filter module

--- lib/smee/filter.ex
+++ lib/smee/filter.ex
@@ -1,22 +1,22 @@
-defmodule Smee.Extract do
+defmodule Smee.Filter do
 
   alias Smee.XSLT
   alias Smee.Metadata
 
-  @list_ids_s File.read! "priv/xslt/list_ids.xsl"
+  def run!(stream) do
+    
+  end
+  
+  def idps(enum, bool) do
+    
+  end
 
-#  def transform(md, stylesheet, params \\ []) do
-#    case XSLT.transform(md.data, stylesheet, params) do
-#      {:ok, xml} -> {:ok, Metadata.update(md, xml)}
-#      {:error, msg} -> {:error, msg}
-#    end
-#  end
+  def sps(enum, bool) do
 
-  def list_ids(md)  do
-    case XSLT.transform(md.data, @list_ids_s, []) do
-      {:ok, xml} -> String.split(xml)
-      {:error, msg} -> []
-    end
+  end
+
+  def aas(enum, bool) do
+
   end
 
   defp wrap_results(results) do
