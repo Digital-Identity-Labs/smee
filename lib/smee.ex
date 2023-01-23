d3ac22c271416d0eab8a31bfe72667f5908a863f
Pete Birkinshaw <pete@binary-ape.org>
2023-01-23 18:39:44 +0000

    Sketch out top-level "easy" functions

--- lib/smee.ex
+++ lib/smee.ex
@@ -3,8 +3,29 @@ defmodule Smee do
   Documentation for `Smee`.
   """
 
-  def entities() do
+  def source(url, type, options \\ []) do
     
   end
+
+  def fetch_metadata(source) do
+    
+  end
+
+  def lookup_entity(source, entity_uri) do
+    
+  end
+  
+  def verify(metadata_or_entity) do
+
+  end
+
+  def list(metadata) do
+
+  end
+
+  def entities(metadata) do
+
+  end
+
   
 end
