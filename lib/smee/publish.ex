be30933a391e90120107fa34df02992d11d96d78
Pete Birkinshaw <pete@binary-ape.org>
2023-01-24 20:57:58 +0000

    Sketching Publish module

---
+++ lib/smee/publish.ex
@@ -0,0 +1,17 @@
+defmodule Smee.Publish do
+
+  alias Smee.Metadata
+  alias Smee.Entity
+  alias Smee.Transform
+
+  import SweetXml
+
+  def single(entity, options \\ []) do
+
+  end
+
+  def aggregate(entities, options \\ []) do
+
+  end
+
+end
