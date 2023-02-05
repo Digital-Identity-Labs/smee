0f6d2f149e77eb1bc4fdbf82f28a901628ca0734
Pete Birkinshaw <pete@binary-ape.org>
2023-02-05 21:43:00 +0000

    Starting docs

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Smee
 
-**TODO: Add description**
+`Smee` is a pragmatic library for handling SAML metadata.
+
+Smee started life as an sprawling mess of code used over five years in an internal application to download and process
+SAML metadata. It's now been extracted, restructured and made a **little** less messy, with the aim of reusing it in other 
+projects.
+
+[![Hex pm](http://img.shields.io/hexpm/v/smee.svg?style=flat)](https://hex.pm/packages/smee)
+[![API Docs](https://img.shields.io/badge/api-docs-yellow.svg?style=flat)](http://hexdocs.pm/smee/)
+![Github Elixir CI](https://github.com/Digital-Identity-Labs/smee/workflows/Elixir%20CI/badge.svg)
+[![License](https://img.shields.io/hexpm/l/smee.svg)](LICENSE)
+
+
+## Features
+
+The top level Smee module contains simplified, top level functions better suited to simpler scripts. Other modules in
+Smee contain more tools for handling SAML metadata, such as:
+
+    * `Smee.Source` - define sources of metadata
+    * `Smee.Metadata` - functions for handling metadata aggregates
+    * `Smee.Entity` - individual SAML entity definitions
+    * `Smee.Extract` - processing metadata to extract information
+    * `Smee.Fetch` - downloading metadata sources
+    * `Smee.MDQ` - functions for MDQ clients (and emulating MDQ clients)
+    * `Smee.Filter` - filtering streams of entity records
+    * `Smee.Transform` - processing and editing entity XML
+    * `Smee.Publish` - Formatting and outputting metadata in various formats
+
+## Examples
+
 
 ## Installation
 
@@ -14,6 +42,20 @@ def deps do
   ]
 end
 ```
+Smee does not do all processing itself, using Elixir - it sometimes cheats (OK, it often cheats) by sending data to
+external programs for processing. At the moment it requires:
+
+* `xmlsec1`
+* `xmllint`
+* `xsltproc`
+
+
+## Purpose
+
+
+## Pragmatic?
+
+## References
 
 * https://shibboleth.atlassian.net/wiki/spaces/SC/pages/1912406916/OAuthRPMetadataProfile
 * https://shibboleth.atlassian.net/wiki/spaces/SC/pages/1836417024/ADFSMetadataProfile
@@ -27,22 +69,17 @@ end
 * https://swamid.se/swamid-metadata.git/tree/xslt
 * https://github.com/ukf/ukf-meta
 
+## Documentation
+
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/Smee>.
 
-```elixir
+## Copyright and License
 
+Copyright (c) 2023 Digital Identity Ltd, UK
 
+Smee is Apache 2.0 licensed.
 
-```
-
-## Extra things needed for mDeco use
-
-* Wrap metadata with namespace declarations
-* Normalise namespaces so they are consistent
-* Maaaaybeeee sign metadata
-* Purge all or some Entity Attributes
-* Insert new Entity Attributes
-* Join metadata fragments together
-
+## Disclaimer
+Smee is not endorsed by The Shibboleth Foundation or any of the NREN's that may have had their XSLT borrowed.
--- lib/smee.ex
+++ lib/smee.ex
@@ -1,6 +1,30 @@
 defmodule Smee do
   @moduledoc """
-  Documentation for `Smee`.
+  Smee is a pragmatic library for handling SAML metadata.
+
+  ## Requirements
+
+
+
+  ## Features
+
+  The top level Smee module contains simplified, top level functions better suited to simpler scripts. Other modules in
+  Smee contain more tools for handling SAML metadata, such as:
+    
+    * `Smee.Source` - define sources of metadata
+    * `Smee.Metadata` - functions for handling metadata aggregates
+    * `Smee.Entity` - individual SAML entity definitions
+    * `Smee.Extract` - processing metadata to extract information
+    * `Smee.Fetch` - downloading metadata sources
+    * `Smee.MDQ` - functions for MDQ clients (and emulating MDQ clients)
+    * `Smee.Filter` - filtering streams of entity records
+    * `Smee.Transform` - processing and editing entity XML
+    * `Smee.Publish` - Formatting and outputting metadata in various formats
+
+  ## Examples
+
+
+
   """
 
   alias Smee.Source
@@ -10,6 +34,19 @@ defmodule Smee do
   alias Smee.Fetch
   alias Smee.MDQ
 
+  @doc """
+    Defines a source of metadata
+
+    Sources of metadata include online aggregate XML, local aggregate files, individual entities, and MDQ services.
+
+  ## Example
+
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      true
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      false
+
+  """
   @spec source(url :: binary()) :: Smee.Source.t()
   def source(url) do
     source(url, type: :aggregate)
@@ -20,6 +57,20 @@ defmodule Smee do
     Source.new(url, options)
   end
 
+  @doc """
+    Downloads a source of metadata and returns a %Metadata{} struct containing XML and information.
+
+    This will contact the remote backend and process the response. An authenticated OTP will produce `true`.
+    Anything other than a success will be returned as `false`.
+
+  ## Example
+
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      true
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      false
+
+  """
   @spec fetch!(source :: binary() | %Source{}) :: Smee.Metadata.t()
   def fetch!(source) when is_binary(source) do
     source(source)
@@ -30,6 +81,20 @@ defmodule Smee do
     Fetch.fetch!(source)
   end
 
+  @doc """
+    Retrieves informatiom for a single entity from an MDQ service (real or emulated)
+
+    This will contact the remote backend and process the response. An authenticated OTP will produce `true`.
+    Anything other than a success will be returned as `false`.
+
+  ## Example
+
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      true
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      false
+
+  """
   @spec lookup!(source :: binary() | %Source{}, entity_id :: binary()) :: Smee.Entity.t()
   def lookup!(source, entity_id) when is_binary(source) do
     source(source)
@@ -40,6 +105,20 @@ defmodule Smee do
     MDQ.lookup(source, entity_id)
   end
 
+  @doc """
+    Lists the IDs of every entity in the metadata.
+
+    This will contact the remote backend and process the response. An authenticated OTP will produce `true`.
+    Anything other than a success will be returned as `false`.
+
+  ## Example
+
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      true
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      false
+
+  """
   @spec entity_ids(source :: binary() | %Source{} | %Metadata{}) :: list(Smee.Entity.t())
   def entity_ids(source) when is_binary(source) do
     source(source)
@@ -56,6 +135,20 @@ defmodule Smee do
     Metadata.entity_ids(metadata)
   end
 
+  @doc """
+    Streams all entities in the specified metadata.
+
+    This will contact the remote backend and process the response. An authenticated OTP will produce `true`.
+    Anything other than a success will be returned as `false`.
+
+  ## Example
+
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      true
+      iex> YubikeyOTP.verify?("ccccccclulvjihcchvujedikcndnbuttfutgvbcgblhk", service)
+      false
+
+  """
   @spec stream_entities(source :: binary() | %Source{} | %Metadata{}) :: %Stream{}
   def stream_entities(source) when is_binary(source) do
     source(source)
