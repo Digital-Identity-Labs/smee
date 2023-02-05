45dd45a03baba8b71bcc8f3299bcc6ba1ff207f9
Pete Birkinshaw <pete@binary-ape.org>
2023-02-05 18:34:30 +0000

    Top level, simpified functions

--- lib/smee.ex
+++ lib/smee.ex
@@ -3,25 +3,67 @@ defmodule Smee do
   Documentation for `Smee`.
   """
 
-  def source(url, type, options \\ []) do
-    
+  alias Smee.Source
+  alias Smee.Metadata
+  alias Smee.Entity
+  alias Smee.Extract
+  alias Smee.Fetch
+  alias Smee.MDQ
+
+  def source(url) do
+    source(url, type: :aggregate)
   end
 
-  def fetch(source) do
-    
+  def source(url, options) do
+    Source.new(url, options)
   end
 
-  def lookup(source, entity_uri) do
-    
+  def fetch!(source) when is_binary(source) do
+    source(source)
+    |> fetch!()
   end
 
-  def entity_ids(metadata) do
-
+  def fetch!(%Source{} = source) do
+    Fetch.fetch!(source)
   end
 
-  def stream_entities(metadata) do
-
+  def lookup!(source, entity_id) when is_binary(source) do
+    source(source)
+    |> lookup!(entity_id)
   end
 
-  
+  def lookup!(%Source{} = source, entity_id) do
+    MDQ.lookup(source, entity_id)
+  end
+
+  def entity_ids(source) when is_binary(source) do
+    source(source)
+    |> fetch!()
+    |> entity_ids()
+  end
+
+  def entity_ids(%Source{} = source) do
+    fetch!(source)
+    |> Metadata.entity_ids()
+  end
+
+  def entity_ids(%Metadata{} = metadata) do
+    Metadata.entity_ids(metadata)
+  end
+
+  def stream_entities(source) when is_binary(source) do
+    source(source)
+    |> fetch!()
+    |> Metadata.stream_entities()
+  end
+
+  def stream_entities(%Source{} = source) do
+    fetch!(source)
+    |> Metadata.stream_entities()
+  end
+
+  def stream_entities(%Metadata{} = metadata) do
+    Metadata.stream_entities(metadata)
+  end
+
 end
