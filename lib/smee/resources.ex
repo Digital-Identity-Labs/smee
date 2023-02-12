5f927a77c0a8e154795b80023f1f91491684f2b2
Pete Birkinshaw <pete@binary-ape.org>
2023-02-12 22:01:18 +0000

    Resources typespecs and tests

--- lib/smee/resources.ex
+++ lib/smee/resources.ex
@@ -2,14 +2,17 @@ defmodule Smee.Resources do
 
   @moduledoc false
 
+  @spec saml_metadata_xml_schema_file() :: binary()
   def saml_metadata_xml_schema_file do
     Path.join(Application.app_dir(:smee, "priv"), "xml_schema/mdqt_check_schema.xsd")
   end
 
+  @spec default_cert_file() ::binary()
   def default_cert_file do
     Application.get_env(:smee, :default_cert_file, CAStore.file_path())
   end
 
+  @spec default_cert_file_url() :: binary()
   def default_cert_file_url do
     "file:" <> default_cert_file
   end
--- test/smee/smee_resources_test.exs
+++ test/smee/smee_resources_test.exs
@@ -3,5 +3,42 @@ defmodule SmeeResourcesTest do
 
   alias Smee.Resources
 
+  describe "saml_metadata_xml_schema_file/0" do
+
+    test "returns the path to the metadata schema file" do
+      path = Resources.saml_metadata_xml_schema_file()
+      assert String.ends_with?(path, "mdqt_check_schema.xsd")
+      assert File.exists?(path)
+    end
+
+    test "returns the path an existing file" do
+      assert File.exists?(Resources.saml_metadata_xml_schema_file())
+    end
+
+  end
+
+  describe "default_cert_file/0" do
+
+    test "returns the path to the default certificate file" do
+      path = Resources.default_cert_file()
+      assert String.ends_with?(path, "cacerts.pem")
+    end
+
+    test "returns the path an existing file" do
+      assert File.exists?(Resources.default_cert_file())
+    end
+
+  end
+
+  describe "default_cert_file_url/0" do
+
+    test "returns the same path as default_cert_file/0, but as a URL" do
+      path = Resources.default_cert_file()
+      url_path = "file:" <> path
+      assert url_path = Resources.default_cert_file_url()
+    end
+
+  end
 
 end
+
