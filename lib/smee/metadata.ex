470cb9953a1cf2f7165a7fed5b78486478660746
Pete Birkinshaw <pete@binary-ape.org>
2023-02-15 12:51:54 +0000

    Types and fixes to Metadata

--- lib/smee/metadata.ex
+++ lib/smee/metadata.ex
@@ -4,13 +4,14 @@ defmodule Smee.Metadata do
   alias Smee.Utils
   alias Smee.Extract
   alias Smee.Entity
+  alias Smee.Metadata
 
   @metadata_types [:aggregate, :single]
 
   @type t :: %__MODULE__{
                downloaded_at: nil | struct(),
                modified_at: nil | struct(),
-               url: binary(),
+               url: nil | binary(),
                id: nil | binary(),
                type: atom(),
                size: integer(),
@@ -28,7 +29,7 @@ defmodule Smee.Metadata do
                cert_url: nil | binary(),
                cert_fingerprint: nil | binary(),
                verified: boolean(),
-               compressed: false,
+               compressed: boolean(),
                changes: integer(),
                priority: integer(),
                trustiness: float()
@@ -46,7 +47,6 @@ defmodule Smee.Metadata do
     :data_hash,
     :etag,
     :label,
-    :entity_count,
     :uri,
     :uri_hash,
     :file_uid,
@@ -54,19 +54,21 @@ defmodule Smee.Metadata do
     :cache_duration,
     :cert_url,
     :cert_fingerprint,
-    :verified,
+    verified: false,
+    entity_count: 0,
     compressed: false,
     changes: 0,
     priority: 5,
     trustiness: 0.5
   ]
 
-  def new(%Stream{} = stream, options \\ []) do
+  @spec new(data :: %Stream{}, options :: keyword()) :: Metadata.t()
+  def new(%Stream{} = data, options \\ []) do
 
     url = Keyword.get(options, :url, nil)
     dlt = DateTime.utc_now()
 
-    data = Smee.Publish.to_xml(stream)
+    data = Smee.Publish.to_xml(data)
     hash = Smee.Utils.sha1(data)
 
     %Metadata{
@@ -93,6 +95,7 @@ defmodule Smee.Metadata do
     |> count_entities()
   end
 
+  @spec new(data :: binary(), options :: keyword()) :: Metadata.t()
   def new(data, options) when is_binary(data) do
 
     url = Keyword.get(options, :url, nil)
@@ -120,11 +123,13 @@ defmodule Smee.Metadata do
 
   end
 
+  # @spec update(metadata :: Metadata.t()) :: Metadata.t()
   def update(metadata) do
     entity = decompress(metadata)
     update(metadata, metadata.data)
   end
 
+  @spec update(metadata :: Metadata.t(), xml :: binary()) :: Metadata.t()
   def update(metadata, xml) do
     changes = metadata.changes + 1
     Map.merge(
@@ -133,19 +138,22 @@ defmodule Smee.Metadata do
     )
   end
 
+  @spec compressed?(metadata :: Metadata.t()) :: boolean()
   def compressed?(metadata) do
     metadata.compressed || false
   end
 
-  def compress(%{compressed: true} = metadata) do
+  @spec compress(metadata :: Metadata.t()) :: Metadata.t()
+  def compress(%Metadata{compressed: true} = metadata) do
     metadata
   end
 
-  def compress(metadata) do
+  def compress(%Metadata{compressed: false} = metadata) do
     metadata
     |> Map.merge(%{data: :zlib.gzip(metadata.data), compressed: true})
   end
 
+  @spec decompress(metadata :: Metadata.t()) :: Metadata.t()
   def decompress(%{compressed: false} = metadata) do
     metadata
   end
@@ -155,38 +163,40 @@ defmodule Smee.Metadata do
     |> Map.merge(%{data: :zlib.gunzip(metadata.data), compressed: false})
   end
 
-  def xml(%{compressed: true} = metadata) do
-    decompress(metadata).data
+  @spec xml(metadata :: Metadata.t()) :: binary()
+  def xml(%Metadata{compressed: true} = metadata) do
+    decompress(metadata)
+    |> xml()
   end
 
   def xml(metadata) do
     metadata.data
   end
 
+  @spec count(metadata :: Metadata.t()) :: integer()
   def count(%Metadata{entity_count: count} = metadata) do
     count || 0
   end
-  
-  def count_entities(metadata) do
-    count = length(String.split(metadata.data, "entityID=\"")) - 1
-    Map.merge(metadata, %{entity_count: count})
-  end
 
+  @spec entity(metadata :: Metadata.t(), uri :: binary()) :: Entity.t()
   def entity(metadata, uri) do
     Extract.entity!(metadata, uri)
   end
 
+  @spec entities(metadata :: Metadata.t()) :: list(Entity.t())
   def entities(metadata) do
     stream_entities(metadata)
     |> Enum.to_list
   end
 
+  @spec stream_entities(metadata :: Metadata.t(), options :: keyword()) :: %Stream{}
   def stream_entities(metadata, options \\ []) do
     options = Keyword.take(options, [:slim, :compress])
     split_to_stream(metadata)
     |> Stream.map(fn xml -> Smee.Entity.derive(xml, metadata, options)  end)
   end
 
+  @spec random_entity(metadata :: Metadata.t()) :: Entity.t()
   def random_entity(%Metadata{entity_count: max} = metadata) do
     if max > 10 do
       uri = Extract.list_ids(metadata)
@@ -239,13 +249,14 @@ defmodule Smee.Metadata do
   def random_entity6(%Metadata{entity_count: max} = metadata) do
     offset = :rand.uniform(max) - 1
     xml = split_to_stream(metadata)
-    |> Stream.drop(offset)
-    |> Stream.take(1)
-    |> Enum.to_list()
-    |> List.first()
+          |> Stream.drop(offset)
+          |> Stream.take(1)
+          |> Enum.to_list()
+          |> List.first()
     Entity.derive(xml, metadata)
   end
-  
+
+  @spec entity_ids(metadata :: Metadata.t()) :: list(binary())
   def entity_ids(%{type: :single} = metadata) do
     extract_id(metadata.data)
   end
@@ -258,6 +269,7 @@ defmodule Smee.Metadata do
     end
   end
 
+  @spec filename(metadata :: Metadata.t()) :: binary()
   def filename(%{uri: uri} = metadata) when not is_nil(uri) do
     filename(metadata, :sha1)
   end
@@ -299,6 +311,13 @@ defmodule Smee.Metadata do
 
   ################################################################################
 
+  @spec count_entities(metadata :: Metadata.t()) :: Metadata.t()
+  defp count_entities(metadata) do
+    count = length(String.split(metadata.data, "entityID=\"")) - 1
+    Map.merge(metadata, %{entity_count: count})
+  end
+
+  @spec extract_info(metadata :: Metadata.t()) :: Metadata.t()
   defp extract_info(%{type: :aggregate} = metadata) do
 
     import SweetXml
@@ -343,6 +362,7 @@ defmodule Smee.Metadata do
     raise "Smee cannot process metadata of type #{metadata.type}!"
   end
 
+  @spec split_to_stream(metadata :: Metadata.t()) :: %Stream{}
   defp split_to_stream(%{type: :aggregate} = metadata) do
     metadata.data
     |> String.splitter("EntityDescriptor>", trim: true)
@@ -363,6 +383,7 @@ defmodule Smee.Metadata do
     Stream.concat([[xml_without_xmlprefix]])
   end
 
+  @spec strip_leading(fx :: binary(), n :: integer) :: binary()
   defp strip_leading(fx, 0) do
     fx
     |> String.split(~r{(<[md:]*EntityDescriptor)}, include_captures: true)
@@ -374,14 +395,14 @@ defmodule Smee.Metadata do
     fx
   end
 
-
-
+  @spec extract_id(xml_fragment :: binary()) :: binary()
   defp extract_id(xml_fragment) do
     import SweetXml
     xml_fragment
     |> xpath(~x"string(/*/@entityID)"s)
   end
 
+  @spec tweak_valid_until(date :: binary() | nil) :: binary() | nil
   defp tweak_valid_until("") do
     nil
   end
@@ -395,23 +416,26 @@ defmodule Smee.Metadata do
     dt
   end
 
+  @spec fix_type(metadata :: Metadata.t()) :: Metadata.t()
   defp fix_type(metadata) do
     type = cond do
-      metadata.type == :mdq && count(metadata) > 1 -> :aggregate
-      metadata.type == :mdq && count(metadata) == 1 -> :single
+      (metadata.type == :mdq) && (count(metadata) > 1) -> :aggregate
+      (metadata.type == :mdq) && (count(metadata) == 1) -> :single
       true -> metadata.type
     end
-    Map.merge(metadata, %{type: type})
+    struct(metadata, %{type: type})
   end
 
-  defp list_ids_int(metadata) do
+  @spec list_ids_int(metadata :: Metadata.t()) :: list(binary())
+  def list_ids_int(metadata) do
     stream_entities(metadata)
     |> Stream.map(fn e -> e.uri end)
-    |> Stream.run
+    |> Enum.to_list
   end
 
-  defp list_ids_ext(md)  do
-    Extract.list_ids(md)
+  @spec list_ids_ext(metadata :: Metadata.t()) :: list(binary())
+  defp list_ids_ext(metadata)  do
+    Extract.list_ids(metadata)
   end
 
 end
