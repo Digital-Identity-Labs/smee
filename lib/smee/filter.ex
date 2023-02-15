299f3f1c72f589ea8f38592159e8a0e3359aed72
Pete Birkinshaw <pete@binary-ape.org>
2023-02-15 13:14:47 +0000

    Use enumerable type for filters

--- lib/smee/filter.ex
+++ lib/smee/filter.ex
@@ -8,27 +8,27 @@ defmodule Smee.Filter do
 #    enum |> Stream.filter(fn e -> xpath(e.xdoc, xpath) == value end)
 #  end
 
-  @spec uri(enum :: %Stream{}, uris :: list(), bool :: boolean() ) :: %Stream{}
+  @spec uri(enum :: Enumerable.t(), uris :: list(), bool :: boolean() ) :: Enumerable.t()
   def uri(enum, uris, bool \\ true) when is_list(uris) do
     enum |> Stream.filter(fn e -> (Enum.member?(uris, e.uri)) == bool end)
   end
 
-  @spec uri(enum :: %Stream{}, uri :: binary(), bool :: boolean() ) :: %Stream{}
+  @spec uri(enum :: Enumerable.t(), uri :: binary(), bool :: boolean() ) :: Enumerable.t()
   def uri(enum, uri, bool ) do
     enum |> Stream.filter(fn e -> (e.uri == uri) == bool end)
   end
 
-  @spec idp(enum :: %Stream{}, bool :: boolean() ) :: %Stream{}
+  @spec idp(enum :: Enumerable.t(), bool :: boolean() ) :: Enumerable.t()
   def idp(enum, bool \\ true) do
     enum |> Stream.filter(fn e -> Entity.idp?(e) == bool end)
   end
 
-  @spec sp(enum :: %Stream{}, bool :: boolean() ) :: %Stream{}
+  @spec sp(enum :: Enumerable.t(), bool :: boolean() ) :: Enumerable.t()
   def sp(enum, bool \\ true) do
     enum |> Stream.filter(fn e -> Entity.sp?(e) == bool end)
   end
 
-  @spec trustiness(enum :: %Stream{}, trustiness :: float(), bool :: boolean() ) :: %Stream{}
+  @spec trustiness(enum :: Enumerable.t(), trustiness :: float(), bool :: boolean() ) :: Enumerable.t()
   def trustiness(enum, trustiness \\ 0.7, bool \\ true) do
     enum |> Stream.filter(fn e -> (Entity.trustiness(e) >= trustiness) == bool end)
   end
