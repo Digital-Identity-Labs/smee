00dd251af9c3ce18e37ab22e552e12273d2d0eac
Pete Birkinshaw <pete@binary-ape.org>
2023-02-24 18:07:05 +0000

    Metadata tests for derive(), not complete but OK

--- test/smee/smee_metadata_test.exs
+++ test/smee/smee_metadata_test.exs
@@ -6,6 +6,7 @@ defmodule SmeeMetadataTest do
   alias Smee.Metadata
   alias Smee.Source
   alias Smee.Fetch
+  alias Smee.Utils
 
   import SweetXml
 
@@ -19,7 +20,6 @@ defmodule SmeeMetadataTest do
   @valid_metadata @valid_metadata_file
                   |> Source.new()
                   |> Fetch.local!()
-
 
   describe "new/2" do
 
@@ -164,10 +164,6 @@ defmodule SmeeMetadataTest do
               )
     end
 
-    test "id can be set using an option" do
-      assert %Metadata{id: "554410"} = Metadata.new(@valid_metadata_xml, id: "554410")
-    end
-
     test "type can be set using an option" do
       assert  %Metadata{type: :single} = Metadata.new(@valid_single_metadata_xml, type: :single)
     end
@@ -218,7 +214,10 @@ defmodule SmeeMetadataTest do
     end
 
     test "hashes cannot be set using an option" do
-      assert %Metadata{data_hash: "7bb9e69f7b5490f679e70b9fc4e4b14d2022ab83"} = Metadata.new(@valid_metadata_xml, data_hash: "WRONG")
+      assert %Metadata{data_hash: "7bb9e69f7b5490f679e70b9fc4e4b14d2022ab83"} = Metadata.new(
+               @valid_metadata_xml,
+               data_hash: "WRONG"
+             )
     end
 
     test "count cannot be set using an option" do
@@ -233,21 +232,245 @@ defmodule SmeeMetadataTest do
       assert %Metadata{verified: false} = Metadata.new(@valid_metadata_xml, verified: true)
     end
 
-#    test "Some incorrect or preliminary types can be fixed automatically" do
-#      assert %Metadata{type: :single} = Metadata.new(@valid_single_metadata_xml, type: :mdq)
-#    end
+    #    test "Some incorrect or preliminary types can be fixed automatically" do
+    #      assert %Metadata{type: :single} = Metadata.new(@valid_single_metadata_xml, type: :mdq)
+    #    end
 
   end
 
   describe "derive/2" do
 
-    #    test "returns a Metadata struct when passed a stream of XML data" do
-    #      assert %Metadata{} = Metadata.new(@valid_metadata_xml)
+    test "returns an :aggregate type Metadata struct when passed a stream of entities" do
+      assert %Metadata{type: :aggregate} = Metadata.derive(Metadata.stream_entities(@valid_metadata))
+    end
+
+    test "returns an :aggregate type Metadata struct when passed a list of entities" do
+      assert %Metadata{type: :aggregate} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    #        test "returns a :single type Metadata struct when passed a single entity" do
+    #          assert %Metadata{type: :single} = Metadata.derive(Metadata.entities(@valid_metadata))
+    #        end
+
+
+    test "uri is set to the URI name of aggregate metadata, if present, entityID of single, or nil if absent" do
+      #      assert %Metadata{uri: "http://example.com/federation"} = Metadata.derive(Metadata.entities(@valid_metadata))
+      #      assert %Metadata{uri: "https://indiid.net/idp/shibboleth"} = Metadata.derive(
+      #               @valid_single_metadata_xml,
+      #               type: :single
+      #             )
+      assert %Metadata{uri: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "uri_hash is set to the sha1 hash of the URI name of the metadata, if present, or nil" do
+      #      assert %Metadata{uri_hash: "797e00e36df8100d422bc6901b21ebf7f8bc58e1"} = Metadata.derive(Metadata.entities(@valid_metadata))
+      #      assert %Metadata{uri_hash: "77603e0cbda1e00d50373ca8ca20a375f5d1f171"} = Metadata.derive(
+      #               @valid_single_metadata_xml,
+      #               type: :single
+      #             )
+      assert %Metadata{uri_hash: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "url defaults to nil" do
+      assert %Metadata{url: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+
+    test "url_hash defaults to nil if no url is set" do
+      assert %Metadata{url_hash: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+
+    test "url_hash is set to sha1 hash of url, if it's set" do
+      assert %Metadata{url_hash: "c28e9841771a33bf7bc9ca45769288de3da9b8b3"} = Metadata.derive(
+               Metadata.entities(@valid_metadata),
+               url: "http://example.com/metadata.xml"
+             )
+    end
+
+    test "data defaults to a trimmed version of passed data param" do
+      data = Metadata.entities(@valid_metadata)
+             |> Smee.Publish.to_xml()
+      assert %Metadata{data: data} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "size is set automatically to the bytesize of the data" do
+      assert %Metadata{size: 39396} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "data_hash is set automatically to the sha1 hash of the data" do
+      data = String.trim(
+        Metadata.entities(@valid_metadata)
+        |> Smee.Publish.to_xml
+      )
+      sha1 = Utils.sha1(data)
+      assert %Metadata{data_hash: sha1} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "type defaults to :aggregate" do
+      assert %Metadata{type: :aggregate} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "downloaded_at defaults to struct creation datetime" do
+      now = DateTime.utc_now()
+      %Metadata{downloaded_at: d_at} = Metadata.derive(Metadata.entities(@valid_metadata))
+      assert DateTime.diff(now, d_at) < 2
+    end
+
+    test "modified_at defaults to struct creation datetime" do
+      now = DateTime.utc_now()
+      %Metadata{modified_at: m_at} = Metadata.derive(Metadata.entities(@valid_metadata))
+      assert DateTime.diff(now, m_at) < 2
+    end
+
+    test "valid_until defaults to the validity in the XML data, or nil" do
+      #      assert %Metadata{valid_until: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+      #      assert %Metadata{valid_until: ~U[2021-12-25 17:33:22.438Z]} = Metadata.derive(
+      #               @valid_single_metadata_xml,
+      #               type: :single
+      #             )
+    end
+
+    test "etag defaults to the data hash" do
+      md = Metadata.derive(Metadata.entities(@valid_metadata))
+      etag = md.etag
+      assert %Metadata{etag: etag} = md
+    end
+
+    test "label defaults to nil" do
+      assert %Metadata{label: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "cert_url defaults to nil" do
+      assert %Metadata{cert_url: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "cert_fingerprint defaults to nil" do
+      assert %Metadata{cert_fingerprint: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "verified defaults to false" do
+      assert %Metadata{verified: false} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "id defaults to nil" do
+      assert %Metadata{id: nil} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "file_uid defaults to '_'" do
+      assert %Metadata{file_uid: "_"} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "entity_count is set to the number of entities in the data" do
+      assert %Metadata{entity_count: 2} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "compressed defaults to false" do
+      assert %Metadata{compressed: false} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "changes defaults to zero" do
+      assert %Metadata{changes: 0} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "priority defaults to 5" do
+      assert %Metadata{priority: 5} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "trustiness defaults to 0.5" do
+      assert %Metadata{trustiness: 0.5} = Metadata.derive(Metadata.entities(@valid_metadata))
+    end
+
+    test "downloaded_at can be set using an option" do
+      now = DateTime.utc_now()
+      assert  %Metadata{downloaded_at: now} = Metadata.derive(Metadata.entities(@valid_metadata), downloaded_at: now)
+    end
+
+    test "modified_at can be set using an option" do
+      now = DateTime.utc_now()
+      assert  %Metadata{modified_at: now} = Metadata.derive(Metadata.entities(@valid_metadata), modified_at: now)
+    end
+
+    test "url can be set using an option" do
+      assert  %Metadata{url: "http://example.com/metadata.xml"} = Metadata.derive(
+                Metadata.entities(@valid_metadata),
+                url: "http://example.com/metadata.xml"
+              )
+    end
+
+    test "etag can be set using an option" do
+      assert  %Metadata{etag: "9EC1E756-5752-4B8E-8261-32B9DD0F2D40"} = Metadata.derive(
+                Metadata.entities(@valid_metadata),
+                etag: "9EC1E756-5752-4B8E-8261-32B9DD0F2D40"
+              )
+    end
+
+    test "label can be set using an option" do
+      assert  %Metadata{label: "Testing Here"} = Metadata.derive(
+                Metadata.entities(@valid_metadata),
+                label: "Testing Here"
+              )
+    end
+
+    #    test "uri can be set using an option" do
+    #      assert  %Metadata{uri: "http://example.com/federation"} = Metadata.derive(
+    #                Metadata.entities(@valid_metadata),
+    #                uri: "http://example.com/federation"
+    #              )
     #    end
-    #
-    #    test "returns a Metadata struct when passed a list of XML data" do
-    #      assert %Metadata{} = Metadata.new(@valid_metadata_xml)
+
+    test "cert_url can be set using an option" do
+      assert %Metadata{cert_url: "http://metadata.ukfederation.org.uk/ukfederation.pem"} = Metadata.derive(
+               Metadata.entities(@valid_metadata),
+               cert_url: "http://metadata.ukfederation.org.uk/ukfederation.pem"
+             )
+    end
+
+    test "cert_fingerprint can be set using an option" do
+      assert %Metadata{cert_fingerprint: "0baa09b8fedaa809bd0e63317afcf3b7a9aedd73"} = Metadata.derive(
+               Metadata.entities(@valid_metadata),
+               cert_fingerprint: "0baa09b8fedaa809bd0e63317afcf3b7a9aedd73"
+             )
+    end
+
+    test "priority can be set using an option" do
+      assert %Metadata{priority: 8} = Metadata.derive(Metadata.entities(@valid_metadata), priority: 8)
+    end
+
+    test "trustiness can be set using an option" do
+      assert %Metadata{trustiness: 0.8} = Metadata.derive(Metadata.entities(@valid_metadata), trustiness: 0.8)
+    end
+
+    test "data cannot be set using an option" do
+      data = String.trim(
+        Metadata.entities(@valid_metadata)
+        |> Smee.Publish.to_xml
+      )
+      assert %Metadata{data: data} = Metadata.derive(Metadata.entities(@valid_metadata), data: "This shouldn't be set")
+    end
+
+    test "hashes cannot be set using an option" do
+      md = Metadata.derive(Metadata.entities(@valid_metadata), data_hash: "WRONG")
+      dhash = md.data_hash
+      refute dhash == "WRONG"
+    end
+
+    test "count cannot be set using an option" do
+      assert %Metadata{entity_count: 2} = Metadata.derive(Metadata.entities(@valid_metadata), entity_count: 20)
+    end
+
+    test "size cannot be set using an option" do
+      assert %Metadata{size: 39396} = Metadata.derive(Metadata.entities(@valid_metadata), size: 100)
+    end
+
+    test "verified cannot be set using an option" do
+      assert %Metadata{verified: false} = Metadata.derive(Metadata.entities(@valid_metadata), verified: true)
+    end
+
+    #    test "Some incorrect or preliminary types can be fixed automatically" do
+    #      assert %Metadata{type: :single} = Metadata.derive(@valid_single_metadata_xml, type: :mdq)
     #    end
+
 
   end
 
