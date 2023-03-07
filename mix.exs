59cc64f3272e891c1d2a3a77d604b38bdcb47063
Pete Birkinshaw <pete@binary-ape.org>
2023-03-07 16:05:42 +0000

    Update mix with hex.pm data, update coveralls to fix bug

--- mix.exs
+++ mix.exs
@@ -6,9 +6,29 @@ defmodule Smxt.MixProject do
       app: :smee,
       version: "0.1.0",
       elixir: "~> 1.14",
+      description: "SAML Metadata Extractor, Etc",
+      package: package(),
+      name: "Smee",
+      source_url: "https://github.com/Digital-Identity-Labs/smee",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      docs: [
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md", "LICENSE"]
+      ],
       deps: deps(),
-      compilers: Mix.compilers() ++ [:rambo]
+      compilers: Mix.compilers() ++ [:rambo], # Needed until issue fixed in Rambo
+      elixirc_paths: elixirc_paths(Mix.env)
     ]
   end
 
@@ -18,10 +38,6 @@ defmodule Smxt.MixProject do
       extra_applications: [:logger]
     ]
   end
-
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support", "priv"]
-  defp elixirc_paths(_), do: ["lib", "priv"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
@@ -37,7 +53,7 @@ defmodule Smxt.MixProject do
 
       {:apex, "~> 1.2", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.13.0", only: :test},
+      {:excoveralls, "~> 0.14 and >= 0.14.4", only: [:dev, :test]},
       {:benchee, "~> 1.0.1", only: [:dev, :test]},
       {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
       {:earmark, "~> 1.3", only: :dev, runtime: false},
@@ -45,4 +61,18 @@ defmodule Smxt.MixProject do
       {:doctor, "~> 0.17.0", only: :dev, runtime: false}
     ]
   end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/Digital-Identity-Labs/smee"
+      }
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support", "priv"]
+  defp elixirc_paths(_), do: ["lib", "priv"]
+
 end
