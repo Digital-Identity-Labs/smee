565aa955412fccf145f0823e41e9c4fbf9a1725f
Pete Birkinshaw <pete@binary-ape.org>
2023-03-13 09:32:49 +0000

    XmlMunger absorbs the splitter code, tweaks and fixes to signature removal and entity munging

--- lib/smee/xml_munger.ex
+++ lib/smee/xml_munger.ex
@@ -7,8 +7,8 @@ defmodule Smee.XmlMunger do
   @xml_declaration ~s|<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n|
   @xml_decl_pattern ~r|^<\?xml.*\?>\n*|ifUm
   @top_tag_pattern ~r|<[md:]*EntityDescriptor.*>|m
-  @uri_extractor_pattern ~r|<[md:]*EntityDescriptor .*entityID="(.+)".*>|im
-  @signature_pattern ~r|<Signature.+?</Signature>|s
+  @uri_extractor_pattern ~r|\A<[md:]*EntityDescriptor.*entityID="(.+)".*>|mUs
+  @signature_pattern ~r|<Signature\s.*>.+</Signature>|ms
 
   @spec prepare_xml(xml :: binary()) :: binary()
   def prepare_xml(xml) do
@@ -45,7 +45,12 @@ defmodule Smee.XmlMunger do
   @spec expand_entity_top(xml :: binary()) :: binary()
   def expand_entity_top(xml, options \\ []) do
 
-    uri = Keyword.get(options, :uri, extract_uri!(xml))
+    xml =
+      prepare_xml(xml)
+      |> remove_xml_declaration()
+
+    uri = Keyword.get(options, :uri, nil)
+    uri = if is_nil(uri), do: extract_uri!(xml), else: uri
     id = Keyword.get(options, :id, nil)
     valid_until = Keyword.get(options, :valid_until, nil)
     id_fragment = if is_nil(id), do: "", else: ~s|ID="#{id}"|
@@ -66,40 +71,89 @@ defmodule Smee.XmlMunger do
 
   @spec shrink_entity_top(xml :: binary()) :: binary()
   def shrink_entity_top(xml, options \\ []) do
-    uri = Keyword.get(options, :uri, extract_uri!(xml))
+
+    xml =
+      prepare_xml(xml)
+      |> remove_xml_declaration()
+
+    uri = Keyword.get(options, :uri, nil)
+    uri = if is_nil(uri), do: extract_uri!(xml), else: uri
     id = Keyword.get(options, :id, nil)
     id_fragment = if is_nil(id), do: "", else: ~s| ID="#{id}"|
 
     replacement_top = ~s|<EntityDescriptor#{id_fragment} entityID="#{uri}">|
 
     xml
-    |> prepare_xml()
-    |> remove_xml_declaration()
     |> String.replace(@top_tag_pattern, replacement_top)
   end
 
   @spec extract_uri!(xml :: binary()) :: binary()
   def extract_uri!(xml) do
-
-    uri = Regex.run(@uri_extractor_pattern, xml, capture: :all_but_first)
-          |> List.first()
-    if uri do
-      uri
-    else
-      raise "Cannot extract URI from XML!"
+    case Regex.run(@uri_extractor_pattern, xml, capture: :all_but_first) do
+      nil -> raise "Cannot extract URI from XML!"
+      [uri] -> uri
     end
   end
 
   @spec remove_signature(xml :: binary()) :: binary()
   def remove_signature(xml) do
     xml
-    |> String.replace(@signature_pattern, "")
+    |> String.replace(@signature_pattern, "\n")
   end
 
   @spec process_entity_xml(xml :: binary()) :: binary()
   def process_entity_xml(xml, options \\ []) do
-    expand_entity_top(xml, options)
+    xml
     |> remove_signature()
+    |> expand_entity_top(options)
+  end
+
+  @spec trim_entity_xml(xml :: binary()) :: binary()
+  def trim_entity_xml(xml, options \\ []) do
+    xml
+    |> remove_signature()
+    |> shrink_entity_top(uri: options[:uri])
+  end
+
+  @spec generate_aggregate_header(options :: keyword()) :: binary()
+  def generate_aggregate_header(options \\ []) do
+
+    """
+    <!--
+    #{aggregate_description(options)}
+    -->
+    <EntitiesDescriptor
+      #{xml_namespace_declarations()}
+      #{id_attrblock(options)} #{cache_attrblock(options)}>
+
+    #{publisher_block(options)}
+
+    """
+  end
+
+  @spec generate_aggregate_footer(options :: keyword()) :: binary()
+  def generate_aggregate_footer(_options) do
+    "\n</EntitiesDescriptor>"
+  end
+
+  @spec split_aggregate_to_stream(xml :: binary(), options :: keyword()) :: Enumerable.t()
+  def split_aggregate_to_stream(xml, options \\ []) do
+    xml
+    |> String.splitter("EntityDescriptor>", trim: true)
+    |> Stream.map(
+         fn xf ->
+           xf <> "EntityDescriptor>"
+           |> String.trim()
+         end
+       )
+    |> Stream.with_index()
+    |> Stream.map(fn {fx, n} -> strip_leading(fx, n) end)
+    |> Stream.reject(fn xf -> String.starts_with?(xf, ["</EntitiesDescriptor>", "</md:EntitiesDescriptor>"]) end)
+  end
+
+  @spec split_single_to_stream(xml :: binary(), options :: keyword()) :: Enumerable.t()
+  def split_single_to_stream(xml, options \\ []) do
+    Stream.concat([[trim_entity_xml(xml, options)]])
   end
 
   ################################################################################
@@ -124,5 +178,72 @@ defmodule Smee.XmlMunger do
     |> Enum.join("\n")
   end
 
+  @spec id_attrblock(options :: keyword()) :: binary()
+  defp id_attrblock(options) do
+    id = Keyword.get(options, :id, "_")
+    name = Keyword.get(options, :name, nil)
+
+    "   " <> if name do
+      ~s(ID="#{id}" Name="#{name}")
+    else
+      ~s|ID="#{id}"|
+    end
+
+  end
+
+  @spec cache_attrblock(options :: keyword()) :: binary()
+  defp cache_attrblock(options) do
+    later = Keyword.get(options, :now, DateTime.utc_now)
+            |> DateTime.add(14, :day)
+            |> DateTime.to_iso8601()
+
+    ~s|cacheDuration="PT6H0M0.000S" validUntil="#{later}"|
+
+  end
+
+  @spec publisher_block(options :: keyword()) :: binary()
+  defp publisher_block(options) do
+
+    pub_uri = Keyword.get(options, :publisher_uri, nil)
+    instant = DateTime.to_iso8601(Keyword.get(options, :now, DateTime.utc_now))
+
+    if pub_uri do
+      """
+      <Extensions>
+      <mdrpi:PublicationInfo creationInstant="#{instant}" publisher="#{pub_uri}"/>
+      </Extensions>
+      """
+    else
+      ""
+    end
+
+  end
+
+  @spec aggregate_uri(options :: keyword()) :: binary() | nil
+  defp aggregate_uri(options) do
+    Keyword.get(options, :uri, nil)
+  end
+
+  @spec aggregate_publisher_uri(options :: keyword()) :: binary() | nil
+  defp aggregate_publisher_uri(options) do
+    Keyword.get(options, :uri, aggregate_uri(options))
+  end
+
+  @spec aggregate_description(options :: keyword()) :: binary() | nil
+  defp aggregate_description(options) do
+    Keyword.get(options, :description, "SAML Aggregate")
+  end
+
+  @spec strip_leading(fx :: binary(), n :: integer) :: binary()
+  defp strip_leading(fx, 0) do
+    fx
+    |> String.split(~r{(<[md:]*EntityDescriptor)}, include_captures: true)
+    |> Enum.drop(1)
+    |> Enum.join()
+  end
+
+  defp strip_leading(fx, _n) do
+    fx
+  end
 
 end
