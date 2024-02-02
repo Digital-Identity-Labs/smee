e89d2f2ecf397d35a0d67282bada72ec5e57a0a0
Pete Birkinshaw <pete@binary-ape.org>
2024-02-02 20:27:15 +0000

    Updated new filters with tests and docs and type specs, oh my

--- lib/smee/filter.ex
+++ lib/smee/filter.ex
@@ -13,6 +13,7 @@ defmodule Smee.Filter do
   """
 
   alias Smee.Entity
+  alias Smee.Utils
 
   #  def xpath(enum, xpath, value) do
   #    enum |> Stream.filter(fn e -> xpath(e.xdoc, xpath) == value end)
@@ -70,8 +71,152 @@ defmodule Smee.Filter do
     |> Stream.filter(fn e -> (Entity.trustiness(e) >= trustiness) == bool end)
   end
 
+  @doc """
+  Filters a stream of entities to include or exclude those that have the specified tag.
+
+  It's best to provide the tag as a string.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec tag(enum :: Enumerable.t(), tag :: binary() | atom(), bool :: boolean()) :: Enumerable.t()
+  def tag(enum, tag, bool \\ true)
+
+  def tag(enum, tag, bool) when is_atom(tag) do
+    tag(enum, Atom.to_string(tag), bool)
+  end
+
+  def tag(enum, tag, bool) do
+    enum
+    |> Stream.filter(fn e -> (tag in Entity.tags(e)) == bool end)
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that have the specified entity category URI.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec entity_category(enum :: Enumerable.t(), category :: binary(), bool :: boolean()) :: Enumerable.t()
+  def entity_category(enum, category, bool \\ true) do
+    enum
+    |> Stream.filter(fn e -> (Enum.find_value(Entity.categories(e), false, fn c -> c == category end)) == bool end)
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that have the specified entity category support URI.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec entity_category_support(enum :: Enumerable.t(), category :: binary(), bool :: boolean()) :: Enumerable.t()
+  def entity_category_support(enum, category, bool \\ true) do
+    enum
+    |> Stream.filter(
+         fn e -> (Enum.find_value(Entity.category_support(e), false, fn c -> c == category end)) == bool end
+       )
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that have the specified assurance profile certification URI.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec assurance(enum :: Enumerable.t(), certification :: binary(), bool :: boolean()) :: Enumerable.t()
+  def assurance(enum, certification, bool \\ true) do
+    enum
+    |> Stream.filter(fn e -> (Enum.find_value(Entity.assurance(e), false, fn c -> c == certification end)) == bool end)
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that were registered at a particular federation, using the
+    specified URI.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec registered_by(enum :: Enumerable.t(), registrar :: binary(), bool :: boolean()) :: Enumerable.t()
+  def registered_by(enum, registrar, bool \\ true) do
+    enum
+    |> Stream.filter(fn e -> (Entity.registration_authority(e) == registrar) == bool end)
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that were registered before the specified date.
+
+  Dates can be Date structs or binary strings in the format "YYYY-MM-DD"
+
+  Entities without a date will be included in the inverted results.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec registered_before(enum :: Enumerable.t(), date :: Date.t() | binary(), bool :: boolean()) :: Enumerable.t()
+  def registered_before(enum, date, bool \\ true) do
+    enum
+    |> Stream.filter(
+         fn e ->
+           (
+             Entity.registered_at(e)
+             |> Utils.before?(date)) == bool
+         end
+       )
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that were registered after the specified date.
+
+  Dates can be Date structs or binary strings in the format "YYYY-MM-DD"
+
+  Entities without a date will be included in the inverted results.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec registered_after(enum :: Enumerable.t(), date :: Date.t() | binary(), bool :: boolean()) :: Enumerable.t()
+  def registered_after(enum, date, bool \\ true) do
+    enum
+    |> Stream.filter(
+         fn e ->
+           (
+             Entity.registered_at(e)
+             |> Utils.after?(date)) == bool
+         end
+       )
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that were registered within the last 7 days.
+
+  Entities without a date will be included in the inverted results.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec new(enum :: Enumerable.t(), bool :: boolean()) :: Enumerable.t()
+  def new(enum, bool \\ true) do
+    enum
+    |> Stream.filter(
+         fn e ->
+           (
+             Entity.registered_at(e)
+             |> Utils.after?(Utils.days_ago(7))) == bool
+         end
+       )
+  end
+
+  @doc """
+  Filters a stream of entities to include or exclude those that were registered within the specified number of days.
+
+  Entities without a date will be included in the inverted results.
+
+  The filter is positive by default but can be inverted by specifying `false`
+  """
+  @spec days(enum :: Enumerable.t(), days :: integer(), bool :: boolean()) :: Enumerable.t()
+  def days(enum, days, bool \\ true) do
+    enum
+    |> Stream.filter(
+         fn e ->
+           (
+             Entity.registered_at(e)
+             |> Utils.after?(Utils.days_ago(days))) == bool
+         end
+       )
+  end
 
   ################################################################################
-
 
 end
--- test/smee/smee_filter_test.exs
+++ test/smee/smee_filter_test.exs
@@ -10,6 +10,18 @@ defmodule SmeeFilterTest do
   @valid_metadata Source.new("test/support/static/aggregate.xml")
                   |> Smee.fetch!()
 
+  @sp_xml File.read! "test/support/static/ukamf_test.xml"
+  @sp_entity Entity.derive(@sp_xml, @valid_metadata)
+
+  @idp_xml File.read! "test/support/static/indiid.xml"
+  @idp_entity Entity.new(@idp_xml)
+
+  @proxy_xml File.read! "test/support/static/cern.xml"
+  @proxy_entity Entity.new(@proxy_xml)
+
+  @local_adfs_xml File.read! "test/support/static/adfs.xml"
+  @local_adfs_entity Entity.new(@local_adfs_xml)
+
   describe "uri/3" do
 
     test "by default only entities with matching entityIDs remain" do
@@ -101,7 +113,284 @@ defmodule SmeeFilterTest do
 
     test "specifying false will invert the filter, so only entities with less trustiness will remain" do
       assert [] = Metadata.stream_entities(@valid_metadata)
-                 |> Filter.trustiness(0.5, false)
+                  |> Filter.trustiness(0.5, false)
+                  |> Enum.to_list()
+    end
+
+  end
+
+  describe "tag/3" do
+
+    test "only entities with a matching tag remain" do
+      tagged_entity = Entity.tag(@sp_entity, "walrus")
+      assert [%Entity{uri: "https://test.ukfederation.org.uk/entity"}] = Stream.concat(
+                                                                           [[tagged_entity, @idp_entity, @proxy_entity]]
+                                                                         )
+                                                                         |> Filter.tag("walrus")
+                                                                         |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      tagged_entity = Entity.tag(@sp_entity, "walrus")
+      assert [
+               %Entity{uri: "https://indiid.net/idp/shibboleth"},
+               %Entity{uri: "https://cern.ch/login"}
+             ] = Stream.concat(
+                   [
+                     [
+                       tagged_entity,
+                       @idp_entity,
+                       @proxy_entity
+                     ]
+                   ]
+                 )
+                 |> Filter.tag(
+                      "walrus",
+                      false
+                    )
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "entity_category/3" do
+
+    test "only entities with a matching entity category remain" do
+      assert [%Entity{uri: "https://cern.ch/login"}] = Stream.concat(
+                                                         [[@sp_entity, @idp_entity, @proxy_entity]]
+                                                       )
+                                                       |> Filter.entity_category(
+                                                            "http://refeds.org/category/research-and-scholarship"
+                                                          )
+                                                       |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"},
+               %Entity{uri: "https://indiid.net/idp/shibboleth"}
+             ] = Stream.concat(
+                   [[@sp_entity, @idp_entity, @proxy_entity]]
+                 )
+                 |> Filter.entity_category("http://refeds.org/category/research-and-scholarship", false)
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "entity_category_support/3" do
+
+    test "only entities with a matching entity category support URI remain" do
+      assert [%Entity{uri: "https://cern.ch/login"}] = Stream.concat(
+                                                         [[@sp_entity, @idp_entity, @proxy_entity]]
+                                                       )
+                                                       |> Filter.entity_category_support(
+                                                            "http://refeds.org/category/research-and-scholarship"
+                                                          )
+                                                       |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"},
+               %Entity{uri: "https://indiid.net/idp/shibboleth"}
+             ] = Stream.concat(
+                   [[@sp_entity, @idp_entity, @proxy_entity]]
+                 )
+                 |> Filter.entity_category_support("http://refeds.org/category/research-and-scholarship", false)
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "assurance/3" do
+
+    test "only entities with a matching assurance URI remain" do
+      assert [%Entity{uri: "https://cern.ch/login"}] = Stream.concat(
+                                                         [[@sp_entity, @idp_entity, @proxy_entity]]
+                                                       )
+                                                       |> Filter.assurance(
+                                                            "https://refeds.org/sirtfi"
+                                                          )
+                                                       |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"},
+               %Entity{uri: "https://indiid.net/idp/shibboleth"}
+             ] = Stream.concat(
+                   [[@sp_entity, @idp_entity, @proxy_entity]]
+                 )
+                 |> Filter.assurance("https://refeds.org/sirtfi", false)
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "registered_by/3" do
+
+    test "only entities with a matching registrar URI remain" do
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"}
+             ] = Stream.concat(
+                   [[@sp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.registered_by(
+                      "http://ukfederation.org.uk"
+                    )
+                 |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      assert [
+               %Entity{uri: "http://adfs.example.ac.uk/adfs/services/trust"},
+               %Entity{uri: "https://cern.ch/login"}
+             ] = Stream.concat(
+                   [[@sp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.registered_by("http://ukfederation.org.uk", false)
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "registered_before/3" do
+
+    test "only entities with a matching registration time will remain" do
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"}
+             ] = Stream.concat(
+                   [[@sp_entity, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.registered_before("2014-01-01")
+                 |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      assert [
+               %Entity{uri: "https://indiid.net/idp/shibboleth"},
+               %Entity{uri: "http://adfs.example.ac.uk/adfs/services/trust"},
+               %Entity{uri: "https://cern.ch/login"}
+             ] = Stream.concat(
+                   [[@sp_entity, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.registered_before("2014-01-01", false)
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "registered_after/3" do
+
+    test "only entities with a matching registration time remain" do
+      assert [
+               %Entity{uri: "https://indiid.net/idp/shibboleth"},
+               %Entity{uri: "https://cern.ch/login"}
+             ] = Stream.concat(
+                   [[@sp_entity, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.registered_after("2014-01-01")
+                 |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"},
+               %Entity{uri: "http://adfs.example.ac.uk/adfs/services/trust"},
+
+             ] = Stream.concat(
+                   [[@sp_entity, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.registered_after("2014-01-01", false)
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "new/2" do
+
+    test "only entities registered in the last week will remain" do
+      two_days_ago = DateTime.utc_now()
+                     |> DateTime.add(-(60 * 60 * 24 * 2))
+                     |> Smee.Utils.format_xml_date()
+      tweaked_sp = String.replace(
+                     @sp_entity.data,
+                     "registrationInstant=\"2012-07-13T11:19:55Z\"",
+                     "registrationInstant=\"#{two_days_ago}\""
+                   )
+                   |> Entity.new()
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"}
+             ] = Stream.concat(
+                   [[tweaked_sp, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.new()
+                 |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      two_days_ago = DateTime.utc_now()
+                     |> DateTime.add(-(60 * 60 * 24 * 2))
+                     |> Smee.Utils.format_xml_date()
+      tweaked_sp = String.replace(
+                     @sp_entity.data,
+                     "registrationInstant=\"2012-07-13T11:19:55Z\"",
+                     "registrationInstant=\"#{two_days_ago}\""
+                   )
+                   |> Entity.new()
+      assert [
+               %Entity{uri: "https://indiid.net/idp/shibboleth"},
+               %Entity{uri: "http://adfs.example.ac.uk/adfs/services/trust"},
+               %Entity{uri: "https://cern.ch/login"}
+             ] = Stream.concat(
+                   [[tweaked_sp, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.new(false)
+                 |> Enum.to_list()
+    end
+
+  end
+
+  describe "days/3" do
+
+    test "only entities registered within the specified number of days remain" do
+      days_ago = DateTime.utc_now()
+                     |> DateTime.add(-(60 * 60 * 24 * 14))
+                     |> Smee.Utils.format_xml_date()
+      tweaked_sp = String.replace(
+                     @sp_entity.data,
+                     "registrationInstant=\"2012-07-13T11:19:55Z\"",
+                     "registrationInstant=\"#{days_ago}\""
+                   )
+                   |> Entity.new()
+      assert [
+               %Entity{uri: "https://test.ukfederation.org.uk/entity"}
+             ] = Stream.concat(
+                   [[tweaked_sp, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.days(16)
+                 |> Enum.to_list()
+    end
+
+    test "specifying false will invert the filter" do
+      days_ago = DateTime.utc_now()
+                     |> DateTime.add(-(60 * 60 * 24 * 14))
+                     |> Smee.Utils.format_xml_date()
+      tweaked_sp = String.replace(
+                     @sp_entity.data,
+                     "registrationInstant=\"2012-07-13T11:19:55Z\"",
+                     "registrationInstant=\"#{days_ago}\""
+                   )
+                   |> Entity.new()
+      assert [
+               %Entity{uri: "https://indiid.net/idp/shibboleth"},
+               %Entity{uri: "http://adfs.example.ac.uk/adfs/services/trust"},
+               %Entity{uri: "https://cern.ch/login"}
+             ] = Stream.concat(
+                   [[tweaked_sp, @idp_entity, @local_adfs_entity, @proxy_entity]]
+                 )
+                 |> Filter.days(16, false)
                  |> Enum.to_list()
     end
 
